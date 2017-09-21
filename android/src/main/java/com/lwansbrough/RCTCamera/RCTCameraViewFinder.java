6c269ca398f8817eb7aacc86a082eb714d45a0b0
Pavol Fulop <pavolfulop@gmail.com>
2017-09-21 17:53:04 +0200

    fix

--- android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
@@ -145,7 +145,7 @@ class RCTCameraViewFinder extends TextureView implements TextureView.SurfaceText
                         options.putInt("type", getCameraType());
                         options.putInt("mode", RCTCameraModule.RCT_CAMERA_CAPTURE_MODE_STILL);
                         options.putInt("orientation", RCTCameraModule.RCT_CAMERA_ORIENTATION_PORTRAIT);
-                        options.putInt("mirrorImage", true);
+                        options.putBoolean("mirrorImage", true);
                         options.putString("quality", RCTCameraModule.RCT_CAMERA_CAPTURE_QUALITY_HIGH);
                         RCTCameraModule.getReactContextSingleton().getNativeModule(RCTCameraModule.class)
                                 .capture(options, new Promise() {
